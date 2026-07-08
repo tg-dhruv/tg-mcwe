b86479a28a78f26bfbbf444b6cb5fc4c00630da7
Rahul P <rahul@gmail.com>
2026-07-08 19:22:23 +0530

    commit1

---
+++ compile-java-executable/49/testcases/tc_01.java
@@ -0,0 +1,28 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import io.testgrid.enums.Alert;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class tc_01 {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void tc_01() {
+		tg.openDevice();
+		tg.wait(3);
+		tg.click("email", 1);
+		tg.close();
+	}
+}
---
+++ compile-java-executable/49/testcases/tc_02.java
@@ -0,0 +1,27 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import io.testgrid.enums.Alert;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class tc_02 {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void tc_02() {
+		tg.openDevice();
+		tg.wait(3);
+		tg.close();
+	}
+}
