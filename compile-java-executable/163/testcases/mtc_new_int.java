e319e146ee6c0502a4b6f597aafd945df74df97a
TestGrid <info@testgrid.io>
2026-08-01 17:05:27 +0530

    test

---
+++ compile-java-executable/163/testcases/mtc_new_int.java
@@ -0,0 +1,33 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class mtc_new_int {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void mtc_new_int() {
+		tg.openBrowser();
+		tg_int var_total = 0;
+		var_total = (int) tg.testFunction("wtf_new", new Object[]{1, 1, 1});
+		tg.printLogs(var_total);
+		tg.close();
+	}
+}
---
+++ compile-java-executable/163/testcases/mtc_new_string.java
@@ -0,0 +1,33 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class mtc_new_string {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void mtc_new_string() {
+		tg.openBrowser();
+		tg_String var_result = "null";
+		var_result = (String) tg.testFunction("wtf_new_string", new Object[]{"a", "b", "c", "d", "e", "f", "g", "h", "i"});
+		tg.printLogs(var_result);
+		tg.close();
+	}
+}
