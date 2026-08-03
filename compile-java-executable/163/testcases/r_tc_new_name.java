242e9233f31cfafc304a2b0e235d80fbf2d7d433
TestGrid <info@testgrid.io>
2026-08-03 16:12:10 +0530

    Test Case Updated

--- compile-java-executable/163/testcases/r_tc_new_name.java
+++ compile-java-executable/163/testcases/r_tc_new_name.java
@@ -26,7 +26,7 @@ public class r_tc_new_name {
 	public void r_tc_new_name() {
 		tg.openBrowser();
 		tg_int var_total = 0;
-		var_total = (int) tg.testFunction("r_tf_new", new Object[]{2, 2, 2, 1});
+		tg.wait("ele_testError", ComparisonType.IS_VISIBLE);
 		tg.printLogs(var_total);
 		tg.close();
 	}
