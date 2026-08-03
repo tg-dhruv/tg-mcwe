33e4b782f5a1687a57bfc95113d6d7e1e55d0e70
TestGrid Admin <info@testgrid.io>
2026-08-03 12:40:30 +0000

    Initial Version Commit

---
+++ compile-java-executable/408/testfunctions/tf_old.java
@@ -0,0 +1,31 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+class tf_old {
+
+	public static Object tf_old(Integer a, Integer b, Integer c) {
+		tg_int var_TGReturn = 0;
+		START_CUSTOM_SCRIPT;
+		var_TGReturn = a+b+c;
+		END_CUSTOM_SCRIPT;
+		return var_TGReturn;
+	}
+}
