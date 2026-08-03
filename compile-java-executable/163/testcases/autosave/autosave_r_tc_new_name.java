7a999f56293bc325ffee470199c12560cd54119d
TestGrid <info@testgrid.io>
2026-08-03 16:10:31 +0530

    Test Function Updated

--- compile-java-executable/163/testcases/autosave/autosave_r_tc_new_name.java
+++ compile-java-executable/163/testcases/autosave/autosave_r_tc_new_name.java
@@ -19,14 +19,15 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-class r_tf_new {
+@Listeners(TestListener.class);
+public class r_tc_new_name {
 
-	public static Object r_tf_new(Integer a, Integer b, Integer c, Integer d) {
-		tg_int var_TGReturn = 0;
-		START_CUSTOM_SCRIPT;
-		var_TGReturn = a+b+c;
-		END_CUSTOM_SCRIPT;
-		tg.wait(1);
-		return var_TGReturn;
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void r_tc_new_name() {
+		tg.openBrowser();
+		tg_int var_total = 0;
+		var_total = (int) tg.testFunction("r_tf_new", new Object[]{2, 2, 2, "2"});
+		tg.printLogs(var_total);
+		tg.close();
 	}
 }
