84f213fd2b1a4e8ee9539f665c84d19539bf3d29
Team Admin <tadmin@testgrid.io>
2026-08-04 20:20:47 +0530

    test

--- compile-java-executable/constants.java
+++ compile-java-executable/constants.java
@@ -1,3 +1,6 @@
 public class constants {
 	public static String var_password = "password@123";
+	public static String var_dhruv = "dddd";
+	public static String var_gmailddd = "hello@gmail.com";
+	public static String var_dd = "dsaf";
 }
