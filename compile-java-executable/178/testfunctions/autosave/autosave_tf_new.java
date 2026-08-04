cf80237eac211c5f2cee0461dbbbe8c715427715
TestGrid <info@testgrid.io>
2026-08-04 16:59:40 +0530

    t

--- compile-java-executable/178/testfunctions/autosave/autosave_tf_new.java
+++ compile-java-executable/178/testfunctions/autosave/autosave_tf_new.java
@@ -19,15 +19,13 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-@Listeners(TestListener.class);
-public class tc_new {
+class tf_new {
 
-	@Test(retryAnalyzer = RetryFailedTestCases.class)
-	public void tc_new() {
-		tg.openBrowser();
-		tg_String var_name = "dhruv";
-		var_name = (String) tg.testFunction("tf_new", new Object[]{"a", "b", "c", "d", "e", "f"});
-		tg.printLogs(var_name);
-		tg.close();
+	public static void tf_new() {
+		tg_String var_TGReturn = "";
+		START_CUSTOM_SCRIPT;
+		var_TGReturn = a+b+c+d;
+		END_CUSTOM_SCRIPT;
+		tg.printLogs(var_TGReturn);
 	}
 }
--- compile-java-executable/178/testfunctions/tf_new_v01.java
+++ compile-java-executable/178/testfunctions/tf_new_v01.java
@@ -19,12 +19,12 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-class tf_new {
+class tf_new_v01 {
 
-	public static Object tf_new(String a, String b, String c, String d) {
+	public static Object tf_new_v01(String a, String b, String c, String d, String e, String f) {
 		tg_String var_TGReturn = "";
 		START_CUSTOM_SCRIPT;
-		var_TGReturn = "hello";
+		var_TGReturn = a+b+c+d+e+f;
 		END_CUSTOM_SCRIPT;
 		tg.printLogs(var_TGReturn);
 		return var_TGReturn;
