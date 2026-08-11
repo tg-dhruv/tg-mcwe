790a112425506315f3b3e4c7bfa304493b5d1959
TestGrid <info@testgrid.io>
2026-08-11 15:45:28 +0530

    Test Function Added

---
+++ compile-java-executable/182/testcases/tc_02.java
@@ -0,0 +1,33 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class tc_02 {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void tc_02() {
+		tg.openBrowser();
+		tg_int var_total = 0;
+		var_total = ((Number) tg.testFunction("tf_01", new Object[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1})).intValue();
+		tg.printLogs(var_total);
+		tg.close();
+	}
+}
