8dc2fd2256081e9e203c007e1ebffacd8417fd28
TestGrid <info@testgrid.io>
2026-09-11 19:51:54 +0530

    t

--- compile-java-executable/198/testfunctions/Testcase44_Step1_5_VerifyEmployee.java
+++ compile-java-executable/198/testfunctions/Testcase44_Step1_5_VerifyEmployee.java
@@ -1,3 +1,22 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.By;
+import java.time.Duration;
+import java.sql.Connection;;
+import java.sql.DriverManager;;
+import java.sql.ResultSet;;
+import java.sql.SQLException;;
+import java.sql.Statement;;
 import io.testgrid.listeners.TestListener;
 import io.testgrid.listeners.RetryFailedTestCases;
 import io.testgrid.tg;
@@ -22,19 +41,21 @@ import org.testng.annotations.Test;
 class testcase44_step1_5_verifyemployee {
 
 	public static void testcase44_step1_5_verifyemployee() {
-		tg.testFunction("login_tg_global_functions");
-		// [DISABLED] tg.type("ele_IchEmployeeIdFKempId_3a11539e", "PHS1REP0044");
-		// [DISABLED] tg.click("ele_IchGoButton_3a11539e", 1);
-		// [DISABLED] tg.wait(15);
-		tg.type("ele_Search_3a2161be", "PHS1REP0044");
-		tg.pressKey(ENTER, 1);
-		tg.wait(15);
-		tg.click("ele_EmployeeInfo_3a2161d2", 1);
-		tg.wait(2);
-		tg.click("ele_EmployeeInfo_3a2161d2", 1);
-		tg.wait(2);
-		tg.click("ele_Hierarchy_3a2161d3", 1);
-		tg.wait(2);
-		tg.click("ele_Hierarchy_3a2161d3", 1);
+				tg.testFunction("login_tg_global_functions", new Object[]{});
+		// [DISABLED] 		tg.type("ele_IchEmployeeIdFKempId_3a11539e", "PHS1REP0044");
+		// [DISABLED] 		tg.click("ele_IchGoButton_3a11539e", 1);
+		// [DISABLED] 		tg.wait(15);
+				tg.type("ele_Search_3a2161be", "PHS1REP0044");
+				tg.pressKey(ENTER, 1);
+				tg.wait(15);
+				tg.click("ele_EmployeeInfo_3a2161d2", 1);
+				tg.wait(2);
+				tg.click("ele_EmployeeInfo_3a2161d2", 1);
+				tg.wait(2);
+				tg.click("ele_Hierarchy_3a2161d3", 1);
+				tg.wait(2);
+				tg.click("ele_Hierarchy_3a2161d3", 1);
+				tg.check.contains("ele_test","ele_test");
+				tg.check.contains("ele_loginLabel",var_As_Of_Date_Time);
 	}
 }
