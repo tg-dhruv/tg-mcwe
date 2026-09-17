56c7a392153f54daac78bc6dbe136c8f92007ba3
TestGrid Admin <info@testgrid.io>
2026-09-17 15:46:14 +0000

    t

--- compile-java-executable/88/testcases/tc_012.java
+++ compile-java-executable/88/testcases/tc_012.java
@@ -21,7 +21,21 @@ public class tc_012 {
 	@Test(retryAnalyzer = RetryFailedTestCases.class)
 	public void tc_012() {
 		tg.openDevice();
-		tg.wait(1);
+				tg.wait("ele_searchproviderlogoImageView1789659770433", ComparisonType.IS_VISIBLE);
+				tg.click("ele_searchproviderlogoImageView1789659770433", 1);
+				tg.wait("ele_SearchGoogleortypeURLEditText1789659774963", ComparisonType.IS_VISIBLE);
+				tg.click("ele_SearchGoogleortypeURLEditText1789659774963", 1);
+				tg.wait("ele_SearchGoogleortypeURLEditText1789659779683", ComparisonType.IS_VISIBLE);
+				tg.click("ele_SearchGoogleortypeURLEditText1789659779683", 1);
+				tg.wait("ele_SearchGoogleortypeURLEditText1789659779683", ComparisonType.IS_VISIBLE);
+				tg.type("ele_SearchGoogleortypeURLEditText1789659779683", "test", true);
+				tg.wait("ele_testTextView1789659788712", ComparisonType.IS_VISIBLE);
+				tg.click("ele_testTextView1789659788712", 1);
+				tg.printLogs("element picker");
+				tg.wait("ele_homebuttonImageButton1789659828953", ComparisonType.IS_VISIBLE);
+				tg.wait("ele_View1789659843957", ComparisonType.IS_VISIBLE);
+				tg.wait("ele_ThereissomethingwrongwiththeproxyserverortheaddressisincorrectTextView1789659860614", ComparisonType.IS_VISIBLE);
+				tg.wait("ele_div085", ComparisonType.IS_VISIBLE);
 		tg.close();
 	}
 }
--- compile-java-executable/92/testcases/test_case_1789611591.java
+++ compile-java-executable/92/testcases/test_case_1789611591.java
@@ -21,7 +21,15 @@ public class test_case_1789611591 {
 	@Test(retryAnalyzer = RetryFailedTestCases.class)
 	public void test_case_1789611591() {
 		tg.openDevice();
-		tg.wait(1);
+				tg.wait("ele_SearchGoogleortypeURLEditText1789659076394", ComparisonType.IS_VISIBLE);
+				tg.click("ele_SearchGoogleortypeURLEditText1789659076394", 1);
+				tg.wait("ele_hyroxcompetitionTextView1789659085603", ComparisonType.IS_VISIBLE);
+				tg.click("ele_hyroxcompetitionTextView1789659085603", 1);
+				tg.printLogs("Element Picker");
+				tg.wait("ele_UseyourGoogleAccountTextView1789659163391", ComparisonType.IS_VISIBLE);
+				tg.wait("ele_NextButton1789659185368", ComparisonType.IS_VISIBLE);
+				tg.printLogs("webview");
+				tg.wait("ele_Australiacardelement14022123889474", ComparisonType.IS_VISIBLE);
 		tg.close();
 	}
 }
