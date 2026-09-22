3ee77e7a70f056e0d8a16dddbaf4c39ed86d391f
TestGrid <info@testgrid.io>
2026-09-22 23:10:21 +0530

    d

--- compile-java-executable/181/testcases/TC_001.java
+++ compile-java-executable/181/testcases/TC_001.java
@@ -36,6 +36,7 @@ public class tc_001 {
 				tg.wait(2);
 				tg.wait("ele_test1122", ComparisonType.IS_VISIBLE);
 				tg.wait("ele_test1122", ComparisonType.IS_VISIBLE);
+				tg.wait("ele_test1122", ComparisonType.IS_VISIBLE);
 		tg.close();
 	}
 }
