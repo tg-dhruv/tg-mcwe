0e894a1d183eca4d5c560d84ba75b4e253eb435c
TestGrid <info@testgrid.io>
2026-09-22 21:57:06 +0530

    s

--- compile-java-executable/181/testcases/login.java
+++ compile-java-executable/181/testcases/login.java
@@ -38,6 +38,7 @@ public class login {
 				tg.wait("ele_trueorigin5172", ComparisonType.IS_VISIBLE, 2);
 				tg.wait("ele_seeeveryda136", ComparisonType.IS_VISIBLE);
 				tg.wait("ele_dhruv123v", ComparisonType.IS_VISIBLE, 2);
+				tg.wait("ele_test222", ComparisonType.IS_VISIBLE, 2);
 		tg.close();
 	}
 }
