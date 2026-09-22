b97ced8c2381e6f045bf6c2b6a9df3d5e6c401d1
TestGrid <info@testgrid.io>
2026-09-22 22:08:53 +0530

    a

--- compile-java-executable/181/testcases/login.java
+++ compile-java-executable/181/testcases/login.java
@@ -33,13 +33,14 @@ public class login {
 	@Test(retryAnalyzer = RetryFailedTestCases.class)
 	public void login() {
 		tg.openBrowser();
-				tg.wait("ele_test11", ComparisonType.IS_VISIBLE);
+				tg.wait("ele_test11aa", ComparisonType.IS_VISIBLE);
 				tg.wait("ele_closefrien3261", ComparisonType.IS_VISIBLE, 3);
 				tg.wait("ele_trueorigin5172", ComparisonType.IS_VISIBLE, 2);
 				tg.wait("ele_seeeveryda136", ComparisonType.IS_VISIBLE);
 				tg.wait("ele_dhruv123v", ComparisonType.IS_VISIBLE, 2);
-				tg.wait("ele_rahul2233dddd", ComparisonType.IS_VISIBLE);
-				tg.wait("ele_rahul2233dddd", ComparisonType.IS_VISIBLE);
+				tg.wait("ele_rahul2233dddd1", ComparisonType.IS_VISIBLE);
+				tg.wait("ele_rahul2233dddd1", ComparisonType.IS_VISIBLE);
+				tg.wait("ele_test11aa", ComparisonType.IS_VISIBLE);
 		tg.close();
 	}
 }
