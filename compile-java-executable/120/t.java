98bb0414a16d3c7b20e5cb5bfdf3d82e086d1a25
TestGrid Admin <info@testgrid.io>
2026-09-23 09:05:02 +0000

    Initial Version Commit

---
+++ compile-java-executable/120/t.java
@@ -0,0 +1,44 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+@Listeners(TestListener.class)
+public class t {
+
+    @Test(retryAnalyzer = RetryFailedTestCases.class)
+    public void facebook_copy() {
+        tg.openBrowser();
+        tg.wait(1);
+        tg.close();
+    }
+
+    @Test(retryAnalyzer = RetryFailedTestCases.class)
+    public void tc01_copy() {
+        tg.openBrowser();
+        tg.wait(1);
+        tg.close();
+    }
+
+    @Test(retryAnalyzer = RetryFailedTestCases.class)
+    public void tc_02_copy() {
+        tg.openBrowser();
+        tg.wait(1);
+        tg.close();
+    }
+}
